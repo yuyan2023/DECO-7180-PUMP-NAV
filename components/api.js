9a5b1dae4619eda7c31db7a10df794332f823ebe
mark-mingze-ma <mingze.ma1@student.uq.edu.au>
2024-09-16 16:26:32 +1000

    update filter popup

--- components/api.js
+++ components/api.js
@@ -2,6 +2,10 @@ const FUEL_DATA_API_SQL = "https://www.data.qld.gov.au/api/3/action/datastore_se
 
 const SQL = {
     allBrand: 'SELECT DISTINCT "Site_Brand" FROM "28ab00ec-00dd-4edf-b272-0543df4dcbe5"',
+    listing: `
+    SELECT "Site_Name", "Fuel_Type", "TransactionDateutc", "Price", "Site_Post_Code", "Sites_Address_Line_1", "Site_Suburb", "Site_Latitude", "Site_Longitude"
+    FROM "28ab00ec-00dd-4edf-b272-0543df4dcbe5" AS main
+`
 };
 
 const fetchData = async data => {
@@ -22,4 +26,8 @@ const fetchAllBrand = async () => {
     return await fetchData({ sql: SQL.allBrand })
 }
 
-export { fetchAllBrand };
+const fetchListing = async () => {
+    return await fetchData({ sql: SQL.listing })
+}
+
+export { fetchAllBrand, fetchListing };
